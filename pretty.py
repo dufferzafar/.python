8cf065787b21dc13e8a753879bd0c2d589540cfd
Shadab Zafar <dufferzafar0@gmail.com>
2015-07-25 01:00:27 +0530

    Pretty JSON: Refactor & Convert to Python 3

--- pretty.py
+++ pretty.py
@@ -6,23 +6,27 @@ Supports:
     Dictionaries (JSON)
 """
 
+from __future__ import print_function
 
-def json(keyvals, indent=''):
+# ANSI color terminal escape sequences
+KEY_COLOR = '[32m'
+VAL_COLOR = '[33m'
+
+
+def json(inp, indent=''):
     """ Print a colored JSON. """
-    # ANSI color terminal escape sequences
-    KEY_COLOR = '[32m'
-    VAL_COLOR = '[33m'
 
-    print '{'
+    print('{')
 
-    for key, val in keyvals.iteritems():
+    for key, val in inp.items():
 
-        print '{}  {}"{}"{}:'.format(indent, KEY_COLOR, key, VAL_COLOR),
+        print('{}  {}"{}"{} : '.format(indent, KEY_COLOR, key, VAL_COLOR), end='')
+
         if isinstance(val, dict):
-            pretty_print(val, indent + '  ')
+            json(val, indent + '  ')
         elif isinstance(val, str):
-            print '"{}",'.format(val)
+            print('"{}",'.format(val))
         else:
-            print '{},'.format(val)
+            print('{},'.format(val))
 
-    print '[0m' + indent + '}'
+    print('[0m' + indent + '}')
